5ead7ee593fbcf658107bc01be2ac1a4b0607aa5
Corey Baker <coreyearleon@icloud.com>
2023-01-27 22:13:29 -0500

    docs

--- Sources/ParseCertificateAuthority/ParseCertificateAuthorityConfiguration.swift
+++ Sources/ParseCertificateAuthority/ParseCertificateAuthorityConfiguration.swift
@@ -8,11 +8,23 @@
 import Foundation
 import ParseSwift
 
+/// The configuratoin for `ParseCertificateAuthority`.
 public struct ParseCertificateAuthorityConfiguration {
+    /// The full URL of the **ca-server** to access the root certificate.
     public internal(set) var caRootCertificateURL: URL
+    /// The full URL of the **ca-server** to access user certificates.
     public internal(set) var caCertificatesURL: URL
+    /// The full URL of the **ca-server** to access users.
     public internal(set) var caUsersPathURL: URL
 
+    /**
+     Creat an instance of the configuration.
+     - parameter caURLString: The full URL string of the **ca-server**.
+     - parameter caRootCertificatePath: The **ca-server** path to access the root certificate.
+     - parameter caCertificatesPath: The **ca-server** path to access user certificates.
+     - parameter caUsersPath: The **ca-server** path to access users.
+     - throws: An error of `ParseError` type.
+     */
     public init(caURLString: String,
                 caRootCertificatePath: String = "/ca_certificate",
                 caCertificatesPath: String = "/certificates/",
